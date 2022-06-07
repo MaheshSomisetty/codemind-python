c5e778a2ca8b2242395da36beb685c9ffa51591d
MaheshSomisetty <97424705+MaheshSomisetty@users.noreply.github.com>
2022-06-07 21:31:20 +0530

    a=input() b=a.lower() c=[] d=97 for i in b:     c.append(ord(i)) c.sort() for i in c:     if i==d:         d=d+1 if(d==123):     print("True") else:     print("False")

---
+++ Pangram_or_not.py
@@ -0,0 +1,15 @@
+a=input()
+b=a.lower()
+c=[]
+d=97
+for i in b:
+    c.append(ord(i))
+c.sort()
+for i in c:
+    if i==d:
+        d=d+1
+if(d==123):
+    print("True")
+else:
+    print("False")
+    
