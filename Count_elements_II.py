ccdc7d9edc861da1d4365799322b4a8cf29c94c0
MaheshSomisetty <97424705+MaheshSomisetty@users.noreply.github.com>
2022-06-09 20:01:40 +0530

    a,b=map(int,input().split()) c=set(map(int,input().split())) d=set(map(int,input().split())) e=c.difference(d) f=d.difference(c) print(len(e)+len(f))

--- Count_elements_II.py
+++ Count_elements_II.py
@@ -1,13 +1,6 @@
 a,b=map(int,input().split())
-c=list(map(int,input().split()))
-d=list(map(int,input().split()))
-e=set(c)
-f=set(d)
-g=0
-for i in e:
-    if i not in f:
-        g+=1
-for i in f:
-    if i not in e:
-        g+=1
-print(g)
+c=set(map(int,input().split()))
+d=set(map(int,input().split()))
+e=c.difference(d)
+f=d.difference(c)
+print(len(e)+len(f))
