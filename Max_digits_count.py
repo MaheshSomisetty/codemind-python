88bad113150b0e81ce5f40a5d2fb643240ecbb99
MaheshSomisetty <97424705+MaheshSomisetty@users.noreply.github.com>
2022-06-09 09:47:30 +0530

    a=int(input()) b=list(map(int,input().split()))[:a] d=[] f=0 for i in b:     c=len(str(i))     d.append(c) e=max(d) for i in b:     if(len(str(i))==e):         f+=1 print(f)

---
+++ Max_digits_count.py
@@ -0,0 +1,12 @@
+a=int(input())
+b=list(map(int,input().split()))[:a]
+d=[]
+f=0
+for i in b:
+    c=len(str(i))
+    d.append(c)
+e=max(d)
+for i in b:
+    if(len(str(i))==e):
+        f+=1
+print(f)
