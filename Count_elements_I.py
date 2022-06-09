760ef2369159bf8d15df3e6efbfd90bd6015d359
MaheshSomisetty <97424705+MaheshSomisetty@users.noreply.github.com>
2022-06-09 19:33:29 +0530

    a,b=map(int,input().split()) c=list(map(int,input().split())) d=list(map(int,input().split())) e=set(c) f=set(d) g=0 for i in e:     if i in f:         g+=1 for i in f:     if i in e:         g+=1 print(g//2)

---
+++ Count_elements_I.py
@@ -0,0 +1,13 @@
+a,b=map(int,input().split())
+c=list(map(int,input().split()))
+d=list(map(int,input().split()))
+e=set(c)
+f=set(d)
+g=0
+for i in e:
+    if i in f:
+        g+=1
+for i in f:
+    if i in e:
+        g+=1
+print(g//2)
