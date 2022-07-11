1caf58264205521e7098f61e685809edc1c7dc60
MaheshSomisetty <97424705+MaheshSomisetty@users.noreply.github.com>
2022-07-11 21:05:25 +0530

    a=input() b=0 for i in a:     if a.count(i)==1:         b+=1 if b==len(a):     print(True) else:     print(False)

--- Isogram_or_not.py
+++ Isogram_or_not.py
@@ -1,9 +1,9 @@
 a=input()
-c=0
+b=0
 for i in a:
-    if(a.count(i)==1):
-        c+=1
-if(c==len(a)):
-    print("True")
+    if a.count(i)==1:
+        b+=1
+if b==len(a):
+    print(True)
 else:
-    print("False")
+    print(False)
