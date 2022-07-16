e20e0e78d4f0350b6c4659407a49b3c10736cf08
MaheshSomisetty <97424705+MaheshSomisetty@users.noreply.github.com>
2022-07-16 10:00:03 +0530

    a=input() a=a.lower() b=[] c='' for i in a:     if a.count(i)==1:         b.append(i) for i in b:     if i==' ':         b.remove(' ') b.sort() for i in b:     c+=i print(len(c))

---
+++ count_unique_characters.py
@@ -0,0 +1,14 @@
+a=input()
+a=a.lower()
+b=[]
+c=''
+for i in a:
+    if a.count(i)==1:
+        b.append(i)
+for i in b:
+    if i==' ':
+        b.remove(' ')
+b.sort()
+for i in b:
+    c+=i
+print(len(c))
