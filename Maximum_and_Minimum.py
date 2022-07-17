926859ec18f63e9b2c506c549a1300bf6bf0af08
MaheshSomisetty <97424705+MaheshSomisetty@users.noreply.github.com>
2022-07-17 11:10:43 +0530

    a=int(input()) b=list(map(int,input().split())) c=[] e=[] for i in b:     if i not in e:         e.append(i) for i in e:     if b.count(i)==i:         c.append(i) if c==[]:     print(-1) else:     print(min(c),max(c))

---
+++ Maximum_and_Minimum.py
@@ -0,0 +1,15 @@
+a=int(input())
+b=list(map(int,input().split()))
+c=[]
+e=[]
+for i in b:
+    if i not in e:
+        e.append(i)
+for i in e:
+    if b.count(i)==i:
+        c.append(i)
+if c==[]:
+    print(-1)
+else:
+    print(min(c),max(c))
+    
