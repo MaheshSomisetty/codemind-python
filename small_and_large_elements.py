df32aa57892eff8162f12f4bf6d49139103e037a
MaheshSomisetty <97424705+MaheshSomisetty@users.noreply.github.com>
2022-07-17 13:09:29 +0530

    a=input() a=a.split() c=[] d=[] for i in a[0]:     c.append(ord(i)) for i in a[-1]:     d.append(ord(i)) print(chr(min(c)),chr(max(d)))

--- small_and_large_elements.py
+++ small_and_large_elements.py
@@ -1,15 +1,9 @@
 a=input()
 a=a.split()
-d=len(a)-1
-b=a[0]
-c=a[d]
-e=[]
-f=[]
-for i in b:
-    e.append(ord(i))
-for i in c:
-    f.append(ord(i))
-g=min(e)
-h=max(f)
-print(chr(g),chr(h))
-
+c=[]
+d=[]
+for i in a[0]:
+    c.append(ord(i))
+for i in a[-1]:
+    d.append(ord(i))
+print(chr(min(c)),chr(max(d)))
