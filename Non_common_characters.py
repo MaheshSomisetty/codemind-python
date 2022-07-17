77c93cb02611eb494af93c4c5e7971a7d8ebb3ee
MaheshSomisetty <97424705+MaheshSomisetty@users.noreply.github.com>
2022-07-17 16:17:05 +0530

    a=input() b=input() a=a.lower() a=a.replace(" ", "") a=list(a) b=b.lower() b=b.replace(" ", "") b=list(b) c=[] x="" y="" for i in a:     if i not in x:         x=x+i for i in b:     if i not in y:         y=y+i for i in x:     if i not in y:         c.append(i) for i in y:     if i not in x:         c.append(i) print(len(c))

---
+++ Non_common_characters.py
@@ -0,0 +1,24 @@
+a=input()
+b=input()
+a=a.lower()
+a=a.replace(" ", "")
+a=list(a)
+b=b.lower()
+b=b.replace(" ", "")
+b=list(b)
+c=[]
+x=""
+y=""
+for i in a:
+    if i not in x:
+        x=x+i
+for i in b:
+    if i not in y:
+        y=y+i
+for i in x:
+    if i not in y:
+        c.append(i)
+for i in y:
+    if i not in x:
+        c.append(i)
+print(len(c))
