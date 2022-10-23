88b1a31ff25b53d6e548844412a0ce9eb75cb96f
MaheshSomisetty <97424705+MaheshSomisetty@users.noreply.github.com>
2022-10-23 13:30:15 +0530

    n,m=map(int,input().split()) a=[] for i in range(n):     a.append(list(map(int,input().split())))
    for i in range(n):
        max=0
        for j in range(m):
            if a[j][i]>max:
                max=a[j][i]
        print(max)

---
+++ Maximum_element_in_each_column.py
@@ -0,0 +1,11 @@
+n,m=map(int,input().split())
+a=[]
+for i in range(n):
+    a.append(list(map(int,input().split())))
+
+for i in range(n):
+    max=0
+    for j in range(m):
+        if a[j][i]>max:
+            max=a[j][i]
+    print(max)
